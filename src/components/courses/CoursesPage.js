e71360d87b3a98fb4469275720ec26ae972806cc
Lin Yi <lin.yi@omnicell.com>
2020-12-24 17:42:29 -0500

    added redux flow

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+
+class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.actions.createCourse(this.state.course);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Course</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+//*
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+//*/
+
+/*
+// declare mapDispatchToProps as an object; all properties are automagically bound to dispatch
+const mapDispatchToProps = {
+  createCourse: courseActions.createCourse
+}
+//*/
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);

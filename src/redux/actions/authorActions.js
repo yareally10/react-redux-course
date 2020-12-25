e9a6a67b25d8ae701c18d1a8640937192f954186
Lin Yi <lin.yi@omnicell.com>
2020-12-25 16:27:38 -0500

    handle api errors

--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -1,6 +1,6 @@
 import * as types from "./actionTypes";
 import * as authorApi from "../../api/authorApi";
-import { beginApiCall } from "./apiStatusActions";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
 
 export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -15,6 +15,7 @@ export function loadAuthors() {
         dispatch(loadAuthorsSuccess(courses));
       })
       .catch((error) => {
+        dispatch(apiCallError(error));
         throw error;
       });
   };
